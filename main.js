a0975b49f32d98d2d01689a9985c9be11d725ca8
yehuda-sanay <yehudasanay1@gmail.com>
2022-06-21 00:17:41 +0300

    21.06.22

--- main.js
+++ main.js
@@ -3,27 +3,25 @@ const array2=[];
 function but(numberValue){
 h1_id.innerHTML+=numberValue;
      array1.push(numberValue);
-console.log(array1);}
+     // console.log(array1);
 
-function shave(){
-array1.forEach(element => {
-     if(element*1==element){
-          array2.push(element);
-          array1.shift(element);
-
+for( var i=0; i<array1.length;i++){
+     if(array1[i].value*1==array1[i].value){
+         var arraynum1= array1.slice(0,i);
+         var arraynum2= array1.slice(i+1,)
      }
-
-});
-console.log(array1);
-console.log(array2);
+console.log(arraynum1);
+console.log(arraynum2);
 }
 
+}
 function cleanScreen(){
      h1_id.innerHTML="";
 }
 
 
 
-// switch(numberValue){
+// switch(i){
 //      case "+":
+
 // }
